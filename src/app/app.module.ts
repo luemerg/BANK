1eb57492d3e28cf722b93edff51933af440052a8
luemerg <luemerg@mail.ru>
2019-04-05 15:53:19 +0300

    START

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HistoryComponent } from './history/history.component';
+import { MainComponent } from './main/main.component';
+
+const routes = [
+  { path: '', component: MainComponent },
+  { path: 'history', component: HistoryComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistoryComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
